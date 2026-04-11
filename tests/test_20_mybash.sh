7feac2c5299c01159f0905311f3c93c337929a22
yunyi1201 <yunyi1201wabu@gmail.com>
2026-04-11 19:42:11 +0800

    rm test_bash.sh

--- tests/test_20_mybash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Test script for exercise 20_mybash
-# Runs mybash with the command script and outputs results
-MYBASH=/workspace/exercises/20_mybash/bin/mybash
-CMD_SCRIPT=/workspace/tests/mybash_cmd.sh
-
-exec "$MYBASH" "$CMD_SCRIPT"
